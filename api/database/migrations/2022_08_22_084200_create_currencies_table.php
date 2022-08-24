a99ddd111c7bb4adb9a5b1ef6fd6c9fa189864d2
K0m414 <komala.kadhiravan@gmail.com>
2022-08-24 10:29:51 +0200

    migrations of currency and pair table

--- api/database/migrations/2022_08_22_084200_create_currencies_table.php
+++ api/database/migrations/2022_08_22_084200_create_currencies_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('currencies', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
+            $table->string('currency_name', 50)->unique();
+            $table->string('currency_code', 3)->unique();
+            $table->string('currency_symbol', 5)->nullable();
+            // $table->double('currency_value', 15, 2);
+            // $table->double('currency_exchange_rate', 15, 2);
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('currencies');
     }
 };
